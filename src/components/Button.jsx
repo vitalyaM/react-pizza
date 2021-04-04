b0cad29487b5afb6677359c29f8658525502ed25
Vitalii Sukhorukov <vitaliysuhorukov123@gmail.com>
2021-04-04 16:38:34 +0300

    feat: add Button, Categories, Header components

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Button = ({ className, outline, children }) => {
+  return (
+    <button
+      className={classNames('button', className, {
+        'button--outline': outline,
+      })}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function Categories({ items, onClickItem }) {
+  const [activeClass, setActiveClass] = useState(0);
+
+  const onSelectItem = (index) => {
+    setActiveClass(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeClass === index ? 'active' : ''}
+              onClick={() => onSelectItem(index)}
+              key={`${name}_${index}`}>
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
