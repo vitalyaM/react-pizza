11dc5c76a321961d3789aba9bb33854316929f22
Vitalii Sukhorukov <vitaliysuhorukov123@gmail.com>
2021-04-15 07:01:50 +0300

    feat: add axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Route } from "react-router-dom";
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
@@ -8,9 +9,14 @@ function App() {
   const [pizzas, setPizzas] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/db.json')
-      .then((resp) => resp.json())
-      .then(json => setPizzas(json.pizzas));
+    axios.get('http://localhost:3000/db.json')
+      .then(({ data }) => {
+        setPizzas(data.pizzas)
+      });
+
+    // fetch('http://localhost:3000/db.json')
+    //   .then((resp) => resp.json())
+    //   .then(json => setPizzas(json.pizzas));
   }, [])
 
   return (
